2a9468f346345270ecffa6d675296017ee4d80c3
Thanh Ha Nguyen <nguyeh83@mcmaster.ca>
2023-07-21 03:58:30 -0400

    menu

---
+++ pages/bangdiemdanh.jsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function bangdiemdanh() {
+    return (
+        <View>
+            <Text> Bảng điểm danh </Text>
+        </View>
+    )
+}

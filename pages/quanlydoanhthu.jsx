18423ec11c770c6c16065bfaca939e8233e0a786
nguy2940 <nhatle.nguyen@mail.utoronto.ca>
2023-07-24 16:12:45 +0700

    ss

--- pages/quanlydoanhthu.jsx
+++ pages/quanlydoanhthu.jsx
@@ -3,9 +3,99 @@ import React from 'react';
 import { VictoryChart, VictoryGroup, VictoryBar, VictoryAxis, VictoryLegend, VictoryPie,
     VictoryContainer } from "victory-native"
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import doanhthu from './doanhthu.json'
 
+const data = {
+    dt: doanhthu.map((d, idx) => ({x: d.ten, y: d.doanhThu})),
+    lg: doanhthu.map((d, idx) => ({x: d.ten, y: d.luong})),
+    ln: doanhthu.map((d, idx) => ({x: d.ten, y: d.loiNhuan}))
+}
 export default function Doanhthu() {
     return (
-        <Text >hahaa</Text>
+        // <Text >hahaa</Text>
+        <ScrollView>
+            <View style={styles.chart}>
+                <VictoryChart style={{parent: {maxWidth: '100%', fontFamily: 'Helvetica Neue'}}}>
+                    <VictoryAxis label="Tháng" style={{tickLabels: { fontSize: 10 }}}></VictoryAxis>
+                    <VictoryAxis label="Tiền (triệu đồng)" dependentAxis style={{tickLabels: { fontSize: 10 }}}></VictoryAxis>
+                    {console.log(doanhthu.slice(0, 3))}
+                    <VictoryGroup offset={20}>
+                        <VictoryBar barRatio={1} data={data.dt.slice(0, 3)} style={{data: {fill: '#E9967A'}}}></VictoryBar>
+                        <VictoryBar barRatio={1} data={data.lg.slice(0,3)} style={{data: {fill: '#ADD8E6'}}}></VictoryBar>
+                        <VictoryBar barRatio={1} data={data.ln.slice(0, 3)} style={{data: {fill: '#FFB6C1'}}}></VictoryBar>
+                    </VictoryGroup>
+                        
+                    
+
+                </VictoryChart>
+            </View>
+        </ScrollView>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      // alignItems: 'center',
+      // justifyContent: 'center',
+      // marginBottom: 400
+    },
+    container1: {
+      marginBottom: 400
+    },
+    button: {
+      position: 'absolute',
+      top: 45,
+      left: 10,
+      padding: 10,
+      zIndex: 1,
+      // fontSize: 27
+    },
+    leaderBoard: {
+      backgroundColor: '#ADD8E6',
+      borderRadius: 5,
+    },
+    bar: {
+      fontSize: 27
+    },
+    header: {
+      position: 'absolute',
+      top: 35, 
+      padding: 20,
+      fontSize: 23,
+      fontWeight: 'bold'  
+    },
+    line: {
+      height: 1,
+      backgroundColor: 'black',
+      width: 500,
+      position: 'absolute',
+      top: 100
+    },
+    circle: {
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      backgroundColor: 'white', 
+    },
+    info: {
+      width: 200,
+      height: 50,
+      borderRadius: 10,
+      backgroundColor: 'white'
+    },
+    title: {
+      marginTop: 50,
+      fontWeight: 'bold',
+      marginLeft: 10,
+      fontSize: 14
+    },
+    chart: {
+      // marginBottom: 200,
+      padding: 0,
+      paddingTop: '10%',
+      borderRadius: 0,
+      width: '100%',
+    }
+  });
+  
